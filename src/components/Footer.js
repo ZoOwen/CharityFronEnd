f5d5d4f788e515f7e11abbd2ee00227ddac19ffb
zoOwen <raiffahmi2001@gmail.com>
2020-05-20 01:37:02 +0700

    data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,7 @@ function Footer() {
             <div className="col-md-3">
               <h4>Info kontak</h4>
               <hr />
-              <p>Phone : 021-123-7890</p>
+              <p>Telepon : 021-123-7890</p>
               <p>E-mail : info@charity.com</p>
               <p>
                 Alamat : Jalan Raya Kemang No.10, Jakarta Selatan,
